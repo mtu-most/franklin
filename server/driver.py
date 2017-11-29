94e31f3228b763e181d51d42d80eaaf5909260ae
Bas Wijnen <wijnen@debian.org>
2017-11-29 18:41:02 +0100

    minor ui fix

--- server/driver.py
+++ server/driver.py
@@ -189,7 +189,7 @@ class Machine: # {{{
 		self.initialized = False
 		self.connected = False
 		self.uuid = config['uuid']
-		self.user_interface = '{Dv2m(No Connection:){dv3m{dv3m{dv3m[0:*Controls:{Dh70%{Dv12m{Dv5m{dh11m(Job Control:)(Buttons:)}(Position:)}{Dh85%(XY Map:)(Z Map:)}}{Dv4m(Abort:){Dv6m(Multipliers:){Dv2m(Gpios:){Dv5m(Temps:)(Temp Graph:)}}}}}Setup:[0:*Profile:(Profile Setup:)Hardware:(Hardware Setup:)Probe:(Probe Setup:)Globals:(Globals Setup:)Axes:(Axis Setup:)Motors:(Motor Setup:)Type:{Dv3m(Type Setup:){Dh50%(Cartesian Setup:){Dh50%(Delta Setup:)(Polar Setup:)}}}Extruder:(Extruder Setup:)Follower:(Follower Setup:)GPIO:(Gpio Setup:)Temps:(Temp Setup:)Pins:(Pin Setup:)]](Confirmation:)}(Message:)}(State:)}}'
+		self.user_interface = '{Dv2m(No Connection:){dv3m{dv3m{dv3m[0:*Controls:{Dh60%{Dv12m{Dv5m{dh11m(Job Control:)(Buttons:)}(Position:)}{Dh85%(XY Map:)(Z Map:)}}{Dv4m(Abort:){Dv6m(Multipliers:){Dv2m(Gpios:){Dv9m(Temps:)(Temp Graph:)}}}}}Setup:[0:*Profile:(Profile Setup:)Hardware:(Hardware Setup:)Probe:(Probe Setup:)Globals:(Globals Setup:)Axes:(Axis Setup:)Motors:(Motor Setup:)Type:{Dv3m(Type Setup:){Dh50%(Cartesian Setup:){Dh50%(Delta Setup:)(Polar Setup:)}}}Extruder:(Extruder Setup:)Follower:(Follower Setup:)GPIO:(Gpio Setup:)Temps:(Temp Setup:)Pins:(Pin Setup:)]](Confirmation:)}(Message:)}(State:)}}'
 		self.pin_names = []
 		self.machine = Driver()
 		self.allow_system = allow_system
